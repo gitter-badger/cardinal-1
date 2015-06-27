7cd1e68ab598beeaa3660245799e699d4859f8a0
Mathew Robinson <mathew.robinson3114@gmail.com>
2015-06-27 15:15:53 -0400

    Update/Create db scripts are now available. Would like to create a script which will configure a completely blank db (add users and create db etc.) however for now this completes issue #8

--- database_setup/hearthstone/update_hs.sh
+++ database_setup/hearthstone/update_hs.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-wget http://hearthstonejson.com/json/AllCards.json
-node updateCards.js
+if[ hash node 2>/dev/null ] then
+    wget http://hearthstonejson.com/json/AllCards.json
+    node updateCards.js
+else
+    echo "Node is required for this operation please install node.js from http://nodejs.org/"
+    exit 1
+fi
