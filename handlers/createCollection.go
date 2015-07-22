c9c37ecc1f5d5faf2fd4d084aa574e42b86d2764
Mathew Robinson <mathew.robinson3114@gmail.com>
2015-07-21 20:50:16 -0400

    Finished tests for signup, login, and sso

--- handlers/createCollection.go
+++ handlers/createCollection.go
@@ -1,20 +1,18 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"gopkg.in/mgo.v2"
 
 	logger "github.com/Sirupsen/logrus"
-	"github.com/chasinglogic/cardinal/cards"
 )
 
 // CreateCollection will add a collection to the appropriate user
 func CreateCollection(w http.ResponseWriter, r *http.Request, db *mgo.Database) {
 	logger.Debug("create collection handler called.")
-	decoder := json.NewDecoder(r.Body)
-	var cc cards.Collection
+	//decoder := json.NewDecoder(r.Body)
+	//var cc cards.Collection
 
-	err := decoder.Decode(&cc)
+	//err := decoder.Decode(&cc)
 }
--- handlers/login_test.go
+++ handlers/login_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,9 +16,17 @@ import (
 func initTestDB() *mgo.Database {
 	var db *mgo.Database
 
-	viper.AddConfigPath("../")
-	viper.SetConfigFile("config.toml")
-	viper.ReadInConfig()
+	configByte, ferr := ioutil.ReadFile("../config.toml")
+	if ferr != nil {
+		fmt.Println(ferr.Error())
+	}
+
+	viper.SetConfigType("toml")
+
+	verr := viper.ReadConfig(bytes.NewReader(configByte))
+	if verr != nil {
+		fmt.Println(verr.Error())
+	}
 
 	dialURL := "mongodb://" + viper.GetString("database.user") + ":" + viper.GetString("database.password") + "@" + viper.GetString("database.ip") + ":" + viper.GetString("database.port") + "/" + viper.GetString("database.name")
 	session, err := mgo.Dial(dialURL)
@@ -62,8 +71,40 @@ func TestLoginHandler(t *testing.T) {
 
 	t.Log(res.Body)
 
-	if res.Code != 302 {
+	if res.Code != 200 {
+		t.Fatalf("Expected: %v: Got: %v", "200", res.Code)
+	}
+}
+
+func TestSSOHandler(t *testing.T) {
+	db := initTestDB()
+
+	mU, mE := json.Marshal(User{Username: "test", Password: []byte("test")})
+	if mE != nil {
+		t.Fatal("Marshal Error")
+	}
+
+	req, _ := http.NewRequest("GET", "/user/login/", bytes.NewReader(mU))
+	res := httptest.NewRecorder()
+
+	LoginHandler(res, req, db.C("users"))
+
+	if res.Code != 200 {
 		t.Fatalf("Expected: %v: Got: %v", "200", res.Code)
 	}
 
+	var umU User
+	umE := json.Unmarshal(res.Body.Bytes(), &umU)
+	if umE != nil {
+		t.Fatalf("Unable to unmarshal response back into a user.")
+	}
+
+	ssoReq, _ := http.NewRequest("POST", "/user/sso?token="+umU.Token, nil)
+	ssoRes := httptest.NewRecorder()
+
+	SSOHandler(ssoRes, ssoReq, db.C("users"))
+
+	if ssoRes.Code != 200 {
+		t.Fatalf("Unexpected response, Expected: 200 Got: %v\nResponse: %v", ssoRes.Code, ssoRes)
+	}
 }
